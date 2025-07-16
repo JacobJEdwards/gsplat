2ea571d8a5b88b8882a0843ecec006419c65db42
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-16 22:11:16 +0100

    correct world model

--- examples/AdaptiveStrategy.py
+++ examples/AdaptiveStrategy.py
@@ -365,9 +365,6 @@ class AdaptiveStrategy(DefaultStrategy):
         gt_img = torch.clamp(info["pixels"], 0.0, 1.0).permute(0, 3, 1, 2)
         current_lpips = self.lpips_metric(rendered_img, gt_img)
 
-        current_scene_encoding = self._get_features_from_graph(params, state, torch.ones(params["means"].shape[0],
-                                                                                         dtype=torch.bool,
-                                                                                         device=params["means"].device))
         while queue and (current_step - queue[0]["step"]) >= self.reward_delay:
             exp = queue.popleft()
 

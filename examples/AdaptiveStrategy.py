430f68b7a863eeb183c8a3e9c61ac6422017d367
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-16 16:50:54 +0100

    features

--- examples/AdaptiveStrategy.py
+++ examples/AdaptiveStrategy.py
@@ -48,7 +48,7 @@ class AdaptiveStrategy(DefaultStrategy):
 
     reward_patch_radius: int = 4
     reward_delay: int = 200
-    max_densification_subset: int = 50_000
+    max_densification_subset: int = 100_000
 
     prune_ac_net: Any = field(default=None, repr=False)
     prune_ac_optimizer: Any = field(default=None, repr=False)
@@ -214,11 +214,11 @@ class AdaptiveStrategy(DefaultStrategy):
         normalized_grads = state["grad2d"] / state["count"].clamp_min(1.0)
         candidate_mask = normalized_grads > self.grow_grad2d
 
-        # if candidate_mask.sum() > self.max_densification_subset:
-        #     candidate_indices = torch.where(candidate_mask)[0]
-        #     rand_indices = torch.randperm(len(candidate_indices), device=device)[:self.max_densification_subset]
-        #     candidate_mask.fill_(False)
-        #     candidate_mask[candidate_indices[rand_indices]] = True
+        if candidate_mask.sum() > self.max_densification_subset:
+            candidate_indices = torch.where(candidate_mask)[0]
+            rand_indices = torch.randperm(len(candidate_indices), device=device)[:self.max_densification_subset]
+            candidate_mask.fill_(False)
+            candidate_mask[candidate_indices[rand_indices]] = True
 
         if candidate_mask.sum() == 0:
             return 0, 0

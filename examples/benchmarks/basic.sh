7b35204da93682ecbe3107a29b287e425a9d3921
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-17 19:51:29 +0100

    contrast dataset

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -14,40 +14,8 @@ do
     echo "Running $SCENE"
 
     # train without eval
-    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py default --eval_steps -1 --disable_viewer --data_factor $DATA_FACTOR \
+    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py default --disable_viewer --data_factor $DATA_FACTOR \
         --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir ../../360_v2/$SCENE/ \
         --result_dir $RESULT_DIR/$SCENE/
-
-    # run eval and render
-    for CKPT in $RESULT_DIR/$SCENE/ckpts/*;
-    do
-        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py default --disable_viewer --data_factor $DATA_FACTOR \
-            --render_traj_path $RENDER_TRAJ_PATH \
-            --data_dir ../../360_v2/$SCENE/ \
-            --result_dir $RESULT_DIR/$SCENE/ \
-            --ckpt $CKPT
-    done
-done
-
-
-for SCENE in $SCENE_LIST;
-do
-    echo "=== Eval Stats ==="
-
-    for STATS in $RESULT_DIR/$SCENE/stats/val*.json;
-    do  
-        echo $STATS
-        cat $STATS; 
-        echo
-    done
-
-    echo "=== Train Stats ==="
-
-    for STATS in $RESULT_DIR/$SCENE/stats/train*_rank0.json;
-    do  
-        echo $STATS
-        cat $STATS; 
-        echo
-    done
 done
